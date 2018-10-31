e1c616894edbe2aa57a2171b88b9c7e50f26f9b8
Olga Lomina <olgalomina@outlook.com>
2018-10-30 21:31:34 -0700

    Add project files.

---
+++ String3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+//3. Write a method to replace all spaces in a string with ‘%20’.
+namespace String3
+{
+    class Program
+    {
+        static string ReplaceSpaces(string str)
+        {
+            StringBuilder sbuild = new StringBuilder();
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == ' ')
+                    sbuild.Append("%20");
+                else
+                    sbuild.Append(str[i]);
+            }
+            
+            return sbuild.ToString();
+        }
+
+        static void Main(string[] args)
+        {
+            string str = "dog abc";
+            str = ReplaceSpaces(str);
+            Console.WriteLine(str);
+        }
+    }
+}
